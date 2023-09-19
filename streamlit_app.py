cf9ca602240d837f7170eab958d7d5c60d00cd7b
zuriich5 <66466697+zuriich5@users.noreply.github.com>
2023-09-19 17:55:27 +0800

    Added dataframe loc function

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,12 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+# Added fruits_selected function name
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+# Added dataframe loc function to enable filtering on our table
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+# streamlit.dataframe(my_fruit_list) , updating the command to the below code to make use of the function
+streamlit.dataframe(fruits_to_show)
