9dee49c047730484872d804c3ab7808142bf9063
zuriich5 <66466697+zuriich5@users.noreply.github.com>
2023-09-19 17:43:58 +0800

    Update streamlit_app.py
    Changed Strawberry to Strawberries

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberry']) 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries']) 
 
 # To be able to choose the fruits by name and not just by number
 my_fruit_list = my_fruit_list.set_index('Fruit')
