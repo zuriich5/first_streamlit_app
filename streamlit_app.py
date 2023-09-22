b688ef1aef5e1ccbd9dd6d97c43e93d3eb3da3b5
zuriich5 <66466697+zuriich5@users.noreply.github.com>
2023-09-22 18:44:02 +0800

    Update streamlit_app.py
    Added indention

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,17 +47,17 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
 
   if not fruit_choice:
-  streamlit.error('Please select a fruit to get information ')
+    streamlit.error('Please select a fruit to get information ')
 
   else
 # Added fruit_choice function that we created to get data from user input
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 # Using pandas library to read the json file 
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 # Added dataframe function to display it as a table
-  streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
   streamlit.error()
